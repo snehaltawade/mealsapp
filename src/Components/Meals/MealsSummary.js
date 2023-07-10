bcc0f5db67d6561ff22dfeacf3ec1a8bc816766d
Deepak Tawade, Snehal <snehal.deepak.tawade@accenture.com>
2023-07-10 12:28:50 +0530

    Updates

--- src/Components/Meals/MealsSummary.js
+++
@@ -1,8 +0,0 @@
-const MealsSummary =()=>(
-    <section className="mealsSummary">
-        <h1>Delicious testy meals</h1>
-        <p>some test about meals.some test about mealssome test about mealssome test about mealssome test about mealssome test about meals</p>
-        <p>some test about meals.some test about meals</p>
-    </section>
-)
-export default MealsSummary;
